fdb444060a0fb8f5ef8b2f7ccba799bc3ccf00b1
harutyun <harutyun.ghubasaryan@betconstruct.com>
2017-03-23 13:15:31 +0400

    Added pagination

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url, include
 import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^index', views.home, name='home'),
+    url(r'^about', views.about, name='about'),
+    url(r'^categories', views.categories, name='categories')
+
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'blog/index.html')
+
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+def categories(request):
+    return render(request, 'blog/categories.html')
